b31430f0d71d1987954f0c67583ce8e2817138ef
MaskedRedstonerProZ <maskedredstonerproz@gmail.com>
2022-11-07 01:15:48 +0100

    - Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/styles-home.css">
+    <title>Textpad: Pocetak</title>
+</head>
+<body>
+    <div id="root-div">
+        <h1>Dobrodosli</h1>
+        <p>Izaberite na koju platformu zelite instalirati server softver:</p>
+        <p id="screen-size"></p>
+        <div>
+            <a href="#"><button>Linux-os</button></a>
+            <a href="#"><button>Mac OS X</button></a>
+            <a href="#"><button>Windows</button></a>
+        </div>
+    </div>
+</body>
+</html>
