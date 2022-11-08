0c49d0bde349269b21a903f1ee8bd4dc3974ee9a
milanradosavac123 <115739830+milanradosavac123@users.noreply.github.com>
2022-11-08 06:45:47 +0100

    added fixes for grammar mistakes in the text

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles/styles.css">
-    <title>Textpad: Pocetak</title>
+    <title>Textpad: Početak</title>
 </head>
 <body>
     <div id="root-div">
-        <h1>Dobrodosli</h1>
-        <p>Izaberite na koju platformu zelite instalirati server softver:</p>
+        <h1>Dobrodošli</h1>
+        <p>Izaberite na koju platformu želite instalirati server softver:</p>
         <p id="screen-size"></p>
         <div>
             <a href="linux-os.php"><button>Linux-os</button></a>
