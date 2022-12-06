fb4b426ca308d4a74df0625bd41dbd50fe309f77
milanradosavac123 <115739830+milanradosavac123@users.noreply.github.com>
2022-12-06 09:17:58 +0100

    Update linux-os.php

--- linux-os.php
+++ linux-os.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles/prism.css">
     <link rel="stylesheet" href="/styles/styles.css">
-    <title>Textpad: Linux-os</title>
+    <title>TextPad: Linux-os</title>
 </head>
 <body id="linux-os-body">
     <script src="scripts/prism.js"></script>
